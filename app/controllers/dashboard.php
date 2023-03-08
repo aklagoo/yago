5d79ac38e200cbdaa2c5421fd604f67296d93abf
Archan Lagoo <aklagoo@gmail.com>
2023-03-08 12:23:24 -0700

    Added comments to dashboard.php

--- app/controllers/dashboard.php
+++ app/controllers/dashboard.php
@@ -1,6 +1,10 @@
 <?php
 
 function removeNumericIndices($arr) {
+    /**
+     * Utility function removing all numbers from an array for simplifying
+     * queries results.
+     */
     foreach ($arr as $key => $value) {
         if (is_int($key)) {
             unset($arr[$key]);
@@ -10,24 +14,30 @@ function removeNumericIndices($arr) {
 }
 
 if($action == 'render') {
-if(!isset($_SESSION['USER_DETAILS'])){
-    header("Location: " . BASE_ADDRESS . "/login");
-    die();
+    /**
+     * Check if the user has logged in with the cafeteria user role. If not
+     * logged in, redirect them to '/login'. If they have logged in but
+     * as a customer, redirect them to the customer menu page at '/'.
+     */
+    if(!isset($_SESSION['USER_DETAILS'])){
+        header("Location: " . BASE_ADDRESS . "/login");
+        die();
+    }
+    if($_SESSION['USER_TYPE']!='cafeteria') {
+        header("Location: " . BASE_ADDRESS);
+        die();
+    }
+    include_once( BASE_PATH . '/views/dashboard.php' );
 }
-if($_SESSION['USER_TYPE']!='cafeteria') {
-    header("Location: " . BASE_ADDRESS);
-    die();
-}
-include_once( BASE_PATH . '/views/dashboard.php' );
 
-}
 elseif($action == 'fetchAll') {
+    // Load all orders for the current user.
     $model = include_once( BASE_PATH . '/models/dashboard.php' );
     $orders    = $model->selectAllOrders();
     $suborders = $model->selectAllSuborders();
 
-    $ajaxResponse = array();
-    $ajaxResponse['orders'] = array();
+    $response = array();
+    $response['orders'] = array();
     $i = 0;
     $search = 0;
 
@@ -36,28 +46,35 @@ elseif($action == 'fetchAll') {
 
         // Add order to the response
         $row['suborders'] = array();
-        array_push($ajaxResponse['orders'], removeNumericIndices($row));
+        array_push($response['orders'], removeNumericIndices($row));
 
-        // Grab all suborders and shove them into the order row
+        /**
+         * Since suborders are sorted by orderID, sequentially iterate and
+         * append until a mismatch is found. 
+         */
         for($j=$search; $j<count($suborders); $j++) {
             if($suborders[$j]['orderID'] !== $orderID) {
                 $search = $j;
                 break;
             }
-            array_push($ajaxResponse['orders'][$i]['suborders'], removeNumericIndices($suborders[$j]));
+            array_push($response['orders'][$i]['suborders'],
+                removeNumericIndices($suborders[$j]));
         }
         $i++;
     }
 
-    $ajaxResponse['foodGroups'] = $model->selectFoodsByGroup();
+    $response['foodGroups'] = $model->selectFoodsByGroup();
+
     // Remove nested indices
-    for($i=0; $i<count($ajaxResponse['foodGroups']); $i++) {
-        $ajaxResponse['foodGroups'][$i] = removeNumericIndices($ajaxResponse['foodGroups'][$i]);
+    for($i=0; $i<count($response['foodGroups']); $i++) {
+        $response['foodGroups'][$i] = removeNumericIndices($response['foodGroups'][$i]);
     }
 
-    echo json_encode($ajaxResponse);
+    echo json_encode($response);
 }
+
 elseif($action == 'paid') {
+    // Mark order as paid.
     if(isset($_POST['orderID'])) {
         $model = include_once( BASE_PATH . '/models/dashboard.php' );
         $model->setPaid($_POST['orderID']);
@@ -65,23 +82,28 @@ elseif($action == 'paid') {
     }
     echo 1;
 }
+
 elseif($action == 'modify') {
-    if(isset($_POST['suborderID']) && isset($_POST['modifier'])) {
-        if($_POST['modifier'] == '+1' || $_POST['modifier'] == '-1'){
-            $model = include_once( BASE_PATH . '/models/dashboard.php' );
-            if($_POST['type']=='collected' || $_POST['type'] == 'prepared'){
-                if($_POST['type']=='collected') {
-                    $model->modifyCollectedSuborder($_POST['suborderID'], $_POST['modifier']);
-                }
-                else{
-                    $model->modifyPreparedSuborder($_POST['suborderID'], $_POST['modifier']);
-                }
-                return 0;
-            }
-        }
+    // Fail if the suborder ID or modifier are not present.
+    if(!isset($_POST['suborderID']) || !isset($_POST['modifier'])) {
         return 1;
     }
-    return 1;
+    // Fail if the modifier is neither '+1' nor '-1'.
+    if($_POST['modifier'] != '+1' || $_POST['modifier'] != '-1') {
+        return 1;
+    } 
+    
+    // Load the model and perform the update.
+    $model = include_once( BASE_PATH . '/models/dashboard.php' );
+    if($_POST['type']=='collected' || $_POST['type'] == 'prepared'){
+        if($_POST['type']=='collected') {
+            $model->modifyCollectedSuborder($_POST['suborderID'], $_POST['modifier']);
+        }
+        else{
+            $model->modifyPreparedSuborder($_POST['suborderID'], $_POST['modifier']);
+        }
+        return 0;
+    }
 }
 
 ?>
