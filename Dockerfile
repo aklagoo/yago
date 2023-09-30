3c671d5b68ac8f1e05749fc916676f1ba56fb7ca
Archan Lagoo <aklagoo@gmail.com>
2023-09-30 09:24:23 -0700

    Fixed database connection

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-FROM php:8.3.0RC3-zts-bookworm
-COPY . /usr/src/app
-WORKDIR /usr/src/app
-CMD [ "php", "index.php" ]
+FROM php:8.3.0RC3-apache-bullseye
+COPY . /var/www/html
+RUN docker-php-ext-install pdo_mysql
--- app/includes/DatabaseEngine.php
+++ app/includes/DatabaseEngine.php
@@ -2,11 +2,12 @@
 
 class DatabaseEngine{
   public function __construct(){
+    $host=$_ENV["DB_HOST"];
     $dbname = $_ENV["DB_NAME"];
     $username = "root";
     $password = $_ENV["DB_PASSWORD"];
     try{
-      $this->connection = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+      $this->connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $e){
